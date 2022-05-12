5e80cebb3e81f982dc4162e6d616c98f3392e83d
BenD13 <105458224+BenD13@users.noreply.github.com>
2022-05-12 15:32:32 -0500

    Add files via upload

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -1,10 +1,9 @@
 # This is a header for the application
 # You should read this header and insert your name and your date below as part of the peer review
 # This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
+# Author: Benjamin Dominguez
+# Creation Date: 05/11/22
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
-
 import random
 import time
 
@@ -14,14 +13,13 @@ def displayIntro():
 	and will share his treasure with you. The other dragon
 	is greedy and hungry, and will eat you on sight.''')
 	print()
-
 def chooseCave():
     cave = ''
-	while cave != '1' and cave != '2':
-		print('Which cave will you go into? (1 or 2)')
-		cave = input()
-
-	return caves
+    while cave != '1' and cave != '2': #Delete spaces and respace with tabs.
+        print('Which cave will you go into? (1 or 2)') #Delete spaces and respace with tabs.
+        cave = input() #Delete spaces and respace with tabs.
+        #Move return line here.
+        return cave #Change caves to cave and delete spaces and respace with tab.
 
 def checkCave(chosenCave):
 	print('You approach the cave...')
@@ -29,7 +27,7 @@ def checkCave(chosenCave):
 	time.sleep(2)
 	print('It is dark and spooky...')
 	#sleep for 2 seconds
-	time.sleep(3)
+	time.sleep(3) #Change time of sleep from 3 to 2
 	print('A large dragon jumps out in front of you! He opens his jaws and...')
 	print()
 	#sleep for 2 seconds
@@ -39,16 +37,15 @@ def checkCave(chosenCave):
 	if chosenCave == str(friendlyCave):
 		print('Gives you his treasure!')
 	else:
-		print 'Gobbles you down in one bite!'
+		print 'Gobbles you down in one bite!' #Add open and close parentheses after print around text.
 
 playAgain = 'yes'
-while playAgain = 'yes' or playAgain = 'y':
+while playAgain = 'yes' or playAgain = 'y': #Change single equal sign to double equal signs.
 	displayIntro()
-	caveNumber = choosecave()
+	caveNumber = choosecave() #Capitalize Cave in choosecave
 	checkCave(caveNumber)
     
 	print('Do you want to play again? (yes or no)')
 	playAgain = input()
 	if playAgain == "no":
-		print("Thanks for planing")
-
+		print("Thanks for planing") #Change planing to playing.
